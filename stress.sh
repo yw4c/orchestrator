6df9cb8f8ce7932b32c7045b4313d10b716504e7
ares.chan.silkrode <ares.chan@silkrode.com.tw>
2020-10-25 12:17:48 +0800

    finish t4est to server

--- stress.sh
+++ stress.sh
@@ -1,5 +1,8 @@
-echo 'GET http://127.0.0.1:7001/booking/sync' | \
-    vegeta attack -rate 10/s -duration 10m | vegeta encode | \
+iso8601=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
+body=body-$iso8601.json
+
+echo 'GET http://104.199.184.127/booking/sync' | \
+    vegeta attack -rate 1 -duration 10m -header=""Host":"orchestrator.com.tw"" | tee ./reports/results-$iso8601.bin | vegeta encode | \
     jaggr @count=rps \
           hist\[100,200,300,400,500\]:code \
           p25,p50,p95:latency \
@@ -8,3 +11,8 @@ echo 'GET http://127.0.0.1:7001/booking/sync' | \
     jplot rps+code.hist.100+code.hist.200+code.hist.300+code.hist.400+code.hist.500 \
           latency.p95+latency.p50+latency.p25 \
           bytes_in.sum+bytes_out.sum
+
+plot=plot-$iso8601.html
+cat ./reports/results-$iso8601.bin | vegeta report
+cat ./reports/results-$iso8601.bin | vegeta plot > ./reports/$plot
+rm -rf ./reports/$body
