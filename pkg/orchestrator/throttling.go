44fa26d1c378d4bed3b615d0a2ba82fb0ee5c3bc
ares.chan <ares.chan@hwaotech.com>
2021-05-03 03:25:28 +0800

    busy wait again

--- pkg/orchestrator/throttling.go
+++ pkg/orchestrator/throttling.go
@@ -2,12 +2,10 @@ package orchestrator
 
 import (
 	"orchestrator/config"
-	"orchestrator/pkg/pkgerror"
 	"sync"
 	"sync/atomic"
 	"time"
 
-	"github.com/rotisserie/eris"
 	"github.com/rs/zerolog/log"
 )
 
@@ -21,52 +19,47 @@ type pendingRequest struct {
 	respChan chan interface{}
 }
 
-func Throttling(requestID string, timeout time.Duration) (err error) {
+type TaskDone func()
+
+func init() {
+	go func(){
+		for atomic.LoadInt32(&handlingCount) >= int32(config.GetConfigInstance().Throttling.Concurrency)
+
+		}
+	}()
+}
+
+func Throttling(requestID string, timeout time.Duration) (err error, callback TaskDone) {
 
 	pending := &pendingRequest{
 		respChan: make(chan interface{}),
 	}
-	pendingRequestMap.Store(requestID, pending)
+	atomic.AddInt32(&handlingCount, 1)
 
-	// keep waiting
+	// waiting if present concurrency count is over than passed concurrency count
 	log.Debug().Str("request ID", requestID).Msg("Waiting Task ")
+	pendingRequestMap.Store(requestID, pending)
+	<-pending.respChan
+	
 
-	for atomic.LoadInt32(&handlingCount) >= int32(config.GetConfigInstance().Throttling.Concurrency) {
-		select {
-		case <-time.Tick(timeout):
-			err = eris.Wrap(pkgerror.ErrTimeout, "async flow handles it too long, let us cancel it")
-			pendingRequestMap.Delete(requestID)
-			return
-		case data := <-pending.respChan:
-			// finished waiting
-			log.Debug().Str("request ID", requestID).Msg("Finished Task")
+	return nil, func() {
+		atomic.AddInt32(&handlingCount, -1)
 
-			// convert to IAsyncFlowContext
-			if d, ok := data.(IAsyncFlowContext); ok {
-				dto = d
-			} else if e, ok := data.(error); ok { // convert to error
-				err = e
-			} else {
-				err = eris.Wrap(pkgerror.ErrInternalError, "convert to IAsyncFlowContext failed ")
-			}
-			pendingRequestMap.Delete(requestID)
-			return
-		}
+		log.Debug().Str("request ID", requestID).Msg("Done Task ")
 	}
-
 }
 
-func TaskDone(requestID string, resp interface{}) error {
+// func TaskDone(requestID string, resp interface{}) error {
 
-	if pending, ok := pendingRequestMap.Load(requestID); ok {
-		if d, ok := pending.(*pendingRequest); ok {
-			d.respChan <- resp
-		} else {
-			return eris.Wrap(pkgerror.ErrInternalError, "convert failed ")
-		}
-	} else {
-		return eris.Wrap(pkgerror.ErrInternalError, "pending request has not been registered")
-	}
-	return nil
+// 	if pending, ok := pendingRequestMap.Load(requestID); ok {
+// 		if d, ok := pending.(*pendingRequest); ok {
+// 			d.respChan <- resp
+// 		} else {
+// 			return eris.Wrap(pkgerror.ErrInternalError, "convert failed ")
+// 		}
+// 	} else {
+// 		return eris.Wrap(pkgerror.ErrInternalError, "pending request has not been registered")
+// 	}
+// 	return nil
 
-}
+// }
--- pkg/orchestrator/throttling_test.go
+++ pkg/orchestrator/throttling_test.go
@@ -5,19 +5,12 @@ import (
 	"sync"
 	"testing"
 	"time"
-
-	"github.com/magiconair/properties/assert"
 )
-
-type mockAsyncDTO2 struct {
-	*AsyncFlowContext
-	foo string
-}
 
 func TestReqWait_Throttling(t *testing.T) {
 
 	var wg sync.WaitGroup
-	var concurrency = 100
+	var concurrency = 10
 	var timeout = 10 * time.Second
 	var finishedCount = 0
 	wg.Add(concurrency)
@@ -28,47 +21,15 @@ func TestReqWait_Throttling(t *testing.T) {
 			reqId := "req" + strconv.Itoa(i)
 
 			// start waiting
-			data, err := Wait(reqId, timeout, func() {
-				wg.Done()
-			})
-
-			if err != nil {
-				t.Log(err.Error())
-				t.Fail()
-			} else {
-				if resp, ok := data.(*mockAsyncDTO); ok {
-					assert.Equal(t, resp.foo, "bar")
-				} else {
-					t.Log("can not parse to mockAsyncDTO")
-					t.Fail()
-				}
-			}
+			_, taskDone := Throttling(reqId, timeout)
+			// do stuff
+			taskDone()
+			t.Log(reqId + "done")
 			finishedCount++
 
 		}(i)
 	}
 	wg.Wait()
-	t.Log("registered")
-
-	for finishedCount < concurrency {
-		t.Log(finishedCount)
-		for i := 1; i <= concurrency; i++ {
-			reqId := "req" + strconv.Itoa(i)
-
-			// mock Msg
-			dao := &mockAsyncDTO{
-				AsyncFlowContext: &AsyncFlowContext{
-					RollbackTopic: "rollback",
-					RequestID:     reqId,
-					CurrentIndex:  0,
-					Topics:        []Topic{"foo"},
-				},
-				foo: "bar",
-			}
-			TaskFinished("req"+strconv.Itoa(i), dao)
-		}
-		time.Sleep(time.Second)
-	}
 
 }
 
